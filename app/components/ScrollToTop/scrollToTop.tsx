7376fb418f3623508530420a79e84c43945b681b
jazzgibran <jazz.gibran@gmail.com>
2024-05-13 17:21:38 +0800

    uppercased

--- app/components/ScrollToTop/scrollToTop.tsx
+++ app/components/ScrollToTop/scrollToTop.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect } from "react";
 
-export default function scrollToTop() {
+export default function ScrollToTop() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
